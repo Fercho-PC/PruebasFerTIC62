f4f33c1e90d532a873b0e81d726a3c358ff377ad
Fercho-PC <fercho20pc@gmail.com>
2021-05-29 17:00:02 -0600

    Update dia 29 de mayo 2021

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import Parrafo from "./components/Parrafo"
+/*import Parrafo from "./components/Parrafo"
 import Parrafo2 from "./components/Parrafo2"
 import Texto from "./components/Texto"
 import Imagen from "./components/Imagen"
-import Pie from "./components/Pie"
+import Pie from "./components/Pie" */
+import Eventos from "./components/Eventos"
+import Suma from "./components/Suma"
+import Variable from "./components/Variable"
 
 
 function App() {
   return (
     <div >
-      <Parrafo2 />
+     {/* <Parrafo2 />
       <Parrafo />
       <Imagen />
       <Texto />
-      <Pie />
+     <Pie /> 
+    <Variable />
+    <Eventos />
+    */}
+    <Suma />
+
     </div>
   );
 }
